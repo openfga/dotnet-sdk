e5e2d8e7e5df85ed7e1ffdf71df3c364799e7a12
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2022-09-30 02:05:20 -0400

    release: v0.1.0

--- src/OpenFga.Sdk/Configuration/Configuration.cs
+++ src/OpenFga.Sdk/Configuration/Configuration.cs
@@ -60,7 +60,7 @@ public class Configuration {
     ///     Version of the package.
     /// </summary>
     /// <value>Version of the package.</value>
-    public const string Version = "0.0.3";
+    public const string Version = "0.1.0";
 
     #endregion Constants
 
