4a697f8829a658e1c280f9fcfbb13fa2a05169de
Raghd Hamzeh <raghd.hamzeh@openfga.dev>
2025-10-24 12:03:38 -0400

    chore: fix retry test to allow for 100ms buffer

--- src/OpenFga.Sdk.Test/Api/OpenFgaApiTests.cs
+++ src/OpenFga.Sdk.Test/Api/OpenFgaApiTests.cs
@@ -2833,10 +2833,8 @@ namespace OpenFga.Sdk.Test.Api {
             var delay1 = attemptTimes[1] - attemptTimes[0];
             var delay2 = attemptTimes[2] - attemptTimes[1];
 
-            Assert.True(delay1 >= 1000, $"First retry delay should be ~1000ms, got {delay1}ms");
-            Assert.True(delay1 < 1300, $"First retry delay should be ~1000ms, got {delay1}ms");
-            Assert.True(delay2 >= 2000, $"Second retry delay should be ~2000ms, got {delay2}ms");
-            Assert.True(delay2 < 2300, $"Second retry delay should be ~2000ms, got {delay2}ms");
+            Assert.InRange(delay1, 900, 1100);
+            Assert.InRange(delay2, 1900, 2100);
         }
 
         [Fact]
